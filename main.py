9f7c2143e2ce0dc6cf0a06d659997f367ffec1d3
Pratham vichare <prathamvichare645@gmail.com>
2025-02-26 15:18:28 -0500

    Data Transformation completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.textSummarizer.logging import logging
 from src.textSummarizer.pipeline.stage_1_pipelinedataIngestion import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.stage_2_pipelinedatatransformation import DataTransformationTrainingPipeline
+
 
 try:
     STAGE_NAME = "data_ingestion"
@@ -12,4 +14,17 @@ except Exception as e:
     logging.error(f"Failed to execute {STAGE_NAME} pipeline")
     logging.error(str(e))
 logging.info("Logging is done")
+
+try: 
+    STAGE_NAME = "data_transformation"
+    logging.info(f"Initiating {STAGE_NAME} pipeline")
+    pipeline = DataTransformationTrainingPipeline()
+    pipeline.initiate_data_transformation()
+    logging.info(f"Completed {STAGE_NAME} pipeline") 
+       
+except Exception as e:
+    logging.error(f"Failed to execute {STAGE_NAME} pipeline")
+    logging.error(str(e))
+logging.info("Logging is done")
+
 
---
+++ src/textSummarizer/components/data_transformation.py
@@ -0,0 +1,29 @@
+import os 
+from src.textSummarizer.logging import logger
+from transformers import AutoTokenizer
+from datasets import load_from_disk
+from src.textSummarizer.entity import DataTransformationConfig
+
+class DataTransformation:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+        self.tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_name)
+
+    def convert_examples_to_features(self, examples):
+        input_encodings = self.tokenizer(examples["dialogue"], truncation=True, padding="max_length", max_length=512)
+
+        with self.tokenizer.as_target_tokenizer():
+             target_encodings = self.tokenizer(examples["summary"], truncation=True, padding="max_length", max_length=150)
+
+        return {
+            'input_ids': input_encodings['input_ids'],
+            'attention_mask': input_encodings['attention_mask'],
+            'labels': target_encodings['input_ids']
+        }
+    
+    def convert(self):
+        dataset_samsum = load_from_disk(self.config.data_path)
+        dataset_samsum_pt= dataset_samsum.map(self.convert_examples_to_features, batched=True)
+        dataset_samsum_pt.save_to_disk(os.path.join(self.config.root_dir, "samsum_pt"))
+        logger.info("Data Transformation process completed successfully")
+        return dataset_samsum_pt
